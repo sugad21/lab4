bd147185d930183a8c914dac1cada23ca8d2e541
sugad21 <sugad21@up.edu>
2019-02-04 22:22:55 -0800

    first one

--- app/src/main/java/edu/up/cs371/epp/photofunpattern/GrayFilter.java
+++ app/src/main/java/edu/up/cs371/epp/photofunpattern/GrayFilter.java
@@ -23,9 +23,26 @@ public class GrayFilter extends PhotoFilter {
     * value
     */
     public int transformPixel(int p0,int p1,int p2,int p3,int p4,int p5,int p6,int p7,int p8) {
-        int intensity = (Color.red(p0) + Color.green(p0) +
-                Color.blue(p0)) / 3;
-        return Color.argb(Color.alpha(p0), intensity,intensity,intensity);
+        /*
+
+
+        p0 = p0/5;
+        p1 = p1/10;
+        p2 = p2/10;
+        p3 = p3/10;
+        p4 = p4/10;
+        p5 = p5/10;
+        p6 = p6/10;
+        p7 = p7/10;
+        p8 = p8/10;
+        */
+        int inPixel = (p0+p1+p2+p3+p4+p5+p6+p7+p8)/9;
+
+        int red = (Color.red(p0)+Color.red(p1)+Color.red(p2)+Color.red(p3)+Color.red(p4)+Color.red(p5)+Color.red(p6)+Color.red(p7)+Color.red(p8))/9;
+        int green = (Color.green(p0)+Color.green(p1)+Color.green(p2)+Color.green(p3)+Color.green(p4)+Color.green(p5)+Color.green(p6)+Color.green(p7)+Color.green(p8))/9;
+        int blue = (Color.blue(p0)+Color.blue(p1)+Color.blue(p2)+Color.blue(p3)+Color.blue(p4)+Color.blue(p5)+Color.blue(p6)+Color.blue(p7)+Color.blue(p8))/9;
+        return Color.argb(Color.alpha(inPixel),red,green,blue);
+
     }
 
 }

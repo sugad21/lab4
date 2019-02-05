9e78d0d3b3f34f03aa94b6927ee0b5bbd483d906
pascuad21 <pascuad21@up.edu>
2019-02-05 00:01:16 -0800

    West Edge Detection Filter Team 2

--- app/src/main/java/edu/up/cs371/epp/photofunpattern/PhotoFun.java
+++ app/src/main/java/edu/up/cs371/epp/photofunpattern/PhotoFun.java
@@ -70,7 +70,7 @@ public class PhotoFun extends AppCompatActivity {
     private class brightnessFilterButtonListener
             implements View.OnClickListener {
         public void onClick(View button) {
-            BrightnessFilter filter = new BrightnessFilter();
+            WestEdgeDetectionFilter filter = new WestEdgeDetectionFilter();
             myNewImageView.setImageBitmap(filter.apply(myOriginalBmp));
         }
     }
--- app/src/main/java/edu/up/cs371/epp/photofunpattern/WestEdgeDetectionFilter.java
+++ app/src/main/java/edu/up/cs371/epp/photofunpattern/WestEdgeDetectionFilter.java
@@ -11,7 +11,7 @@ import android.graphics.Color;
  *  https://github.com/edcepp/PhotoFunPattern
  */
 
-public class BrightnessFilter extends PhotoFilter {
+public class WestEdgeDetectionFilter extends PhotoFilter {
 
     private final int ADJUSTMENT = 100;
 
@@ -30,23 +30,23 @@ public class BrightnessFilter extends PhotoFilter {
         int blue = constrain(Color.blue(inPixel) + ADJUSTMENT);
         int outPixel = Color.argb(Color.alpha(inPixel), red, green, blue);
  */
-/**
-    int p4 = (inPixel * -2);
-     p0 = inPixel;
-     p1 = inPixel;
-     p2 = (inPixel * -1);
-     p3 = inPixel;
-     p5 = (inPixel * -1);
-     p6 = inPixel;
-     p7 = inPixel;
-     p8 = (inPixel * -1);
-    int P4 = ( p0 + p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 ) / 9 ;
-*/
 
-        int red = constrain(Color.red(inPixel) + ADJUSTMENT);
-        int green = constrain(Color.green(inPixel) + ADJUSTMENT);
-        int blue = constrain(Color.blue(inPixel) + ADJUSTMENT);
-        int outPixel = Color.argb(Color.alpha(inPixel), red, green, blue);
+    int p4 = (inPixel * -2);
+     //p0 = inPixel;
+     //p1 = inPixel;
+     p2 = (p2 * -1);
+     //p3 = inPixel;
+     p5 = (p5 * -1);
+     //p6 = inPixel;
+     //p7 = inPixel;
+     p8 = (p8 * -1);
+    int P4 = ( p0 + p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 ) / 9 ;
+
+
+        int red = constrain((Color.red(p0)+Color.red(p1)+Color.red(p2)+Color.red(p3)+Color.red(p4)+Color.red(p5)+Color.red(p6)+Color.red(p7)+Color.red(p8))/9);
+        int green = constrain((Color.green(p0)+Color.green(p1)+Color.green(p2)+Color.green(p3)+Color.green(p4)+Color.green(p5)+Color.green(p6)+Color.green(p7)+Color.green(p8))/9);
+        int blue = constrain((Color.blue(p0)+Color.blue(p1)+Color.blue(p2)+Color.blue(p3)+Color.blue(p4)+Color.blue(p5)+Color.blue(p6)+Color.blue(p7)+Color.blue(p8))/9);
+        int outPixel = Color.argb(Color.alpha(P4), red, green, blue);
         return outPixel;
     }
 
